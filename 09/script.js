e2766548b1668f86cc6ba4af596cbcabf12708de
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-09-03 22:56:37 +0530

    09

---
+++ 09/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-n",
+        prevEl: ".swiper-button-p",
+    },
+});
